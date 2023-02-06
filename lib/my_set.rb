d6d0afcc6d0c6f1412009529e5a932e3508a5c8a
Christian Castillo <castillochristian3@gmail.com>
2023-02-06 17:42:51 -0500

    commit tree cbde038906df61acecd8ef35da296d0f854bcd76

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,49 @@
-# your code here
+class MySet
+    attr_reader :hash
+  
+    def initialize(enumerable = [])
+      @hash = Hash[enumerable.map { |value| [value, true] }]
+    end
+  
+    def include?(value)
+      hash.key?(value)
+    end
+  
+    def add(value)
+      hash[value] = true
+      self
+    end
+  
+    def delete(value)
+      hash.delete(value)
+      self
+    end
+  
+    def size
+      hash.size
+    end
+  
+    def self.[](*enumerable)
+      new(enumerable)
+    end
+  
+    def clear
+      hash.clear
+      self
+    end
+
+    def each
+        hash.each_key do |key|
+            yield key
+        end
+        self
+    end
+
+    def inspect
+        "#<MySet: {#{hash.keys.join(", ")}}>"
+    end
+
+  end
+  
+  set = MySet.new([1, 2, 3, 3])
+  
